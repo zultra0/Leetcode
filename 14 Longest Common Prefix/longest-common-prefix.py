9cb3fd8746c57ef9e5ee447e8c4a30343f6eb5e0
Mikhail <mikhail.manco.dev@gmail.com>
2026-01-11 18:43:46 -0500

    Update for: Code challenge submission for question: 14 Longest Common Prefix

--- 14 Longest Common Prefix/longest-common-prefix.py
+++ 14 Longest Common Prefix/longest-common-prefix.py
@@ -2,13 +2,13 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs: return ""
         prefix = strs[0] 
-        i = 1
-        
-        while i < len(strs): 
+        i = 1 
+        while i < len(strs):
             j = 0
-            while j < len(prefix) and j < len(strs[i]) and prefix[j] == strs[i][j]: 
-                j += 1
+            while j < len(prefix) and j < len(strs[i]) and prefix[j] == strs[i][j]:
+                j = j + 1
             prefix = prefix[:j]
-            i += 1
+            i = i + 1
         return prefix
